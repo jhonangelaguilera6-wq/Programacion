f0027844edd2eae768cdd5d4d83887e3a334b9d4
Dariam <Jhonangelaguilera6@gmail.com>
2026-01-11 12:32:20 +0100

    commit inicial

---
+++ Entrenamiento.cpp
@@ -0,0 +1,77 @@
+function Ir_al_supermercado():
+1. Levantarse de la cama.
+2. colocar las piernas en el suelo.
+3. caminar hacia la puerta.
+4. extender la mano. 
+5. Girar la manilla de la puerta.
+6. Jalar la puerta.
+7. Pasar la puerta.
+8. Extender el brazo.
+9. Cerrar la puerta.
+10. Caminar hacia la puerta de la casa.
+11. extender la mano. 
+12. Girar la manilla de la puerta.
+13. Jalar la puerta.
+14. Pasar la puerta.
+15. Cerrar la puerta.
+16. Caminar hacia el coche.
+17. Extender la mano.
+18. abrir la puerta del coche.
+19. Sentarse en el asiento del conductor.
+20. Cerrar la puerta.
+21. Meter la llave en el coche.
+22. Encender el coche.
+23. Colocar las manos en el volante.
+24. Colocar el coche en retroseso.
+25. Acelerar el coche.
+
+
+Ir_al_supermercado()
+1. Salir de la habitacion
+2. Correr hacia la puerta.
+3. Patear la puerta.
+4. Saltar hacia el coche.
+5. Romper el parabrisas.
+6. Meter la llave
+7. Acelerar el coche.
+
+class Personas {
+	edad: integer
+	nombre: String
+	color_ojos: String
+	decirNombre()
+		habla {tu nombre}
+	}
+	
+personaAfricana = new Personas (
+	edad: 22
+	nombre: "Mohammed"
+	color_ojos: "MarronOscuros"
+)
+
+personaGabriel = new Personas (
+	edad: 39
+	nombre: "Gabriel"
+	color_ojos: "azulCielo"
+)
+
+function Ir_al_supermercado(persona)
+	si persona.edad > 22:
+		Abrir la puerta.
+		Salir de la habitacion
+		Correr hacia la puerta.
+		Patear la puerta.	
+		Saltar hacia el coche.
+		Romper el parabrisas.
+		Meter la llave
+		Acelerar el coche.
+	sino: 
+		vete de aqui!
+		
+	function Ir_al_supermercado(persona)
+		Correr hacia la entrada
+		Salir de la habitacion
+	
+		
+		
+
